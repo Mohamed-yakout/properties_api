6d200eb36a1ac8affa753a8c3a1a2ba1c636bfdc
Mohamed Yakout <mohamed.yakout91@gmail.com>
2021-11-12 23:25:53 +0400

    customize routes to only index action

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -8,47 +8,7 @@ class PropertiesController < ApplicationController
     render json: @properties
   end
 
-  # GET /properties/1
-  def show
-    render json: @property
-  end
-
-  # POST /properties
-  def create
-    @property = Property.new(property_params)
-
-    if @property.save
-      render json: @property, status: :created, location: @property
-    else
-      render json: @property.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /properties/1
-  def update
-    if @property.update(property_params)
-      render json: @property
-    else
-      render json: @property.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /properties/1
-  def destroy
-    @property.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def property_params
-      params.fetch(:property, {})
-    end
-
     # location params is mandatory in filter
     def location_filter_params
       params.slice(:lat, :lng)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :properties
+  resources :properties, only: [:index]
+
+  root 'properties#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
