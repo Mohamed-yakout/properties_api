f3cc66224413a097d67c6a72aa51927d5a31cc5e
Mohamed Yakout <mohamed.yakout91@gmail.com>
2021-11-13 15:17:47 +0400

    Use Validates class for validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::BadRequest, with: :bad_request_error
+
+  # Render exceptions errors
+  def record_not_found(exception)
+    render json: {message: exception.message}, status: :not_found
+    return
+  end
+
+  def bad_request_error(exception)
+    render json: {error: exception.message}, status: :bad_request
+    return
+  end
 end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,9 +1,15 @@
 class PropertiesController < ApplicationController
+  before_action :check_search_params
+
   # GET /properties
   def index
     @properties = Property.filter(property_filter_params).filter_by_location(location_filter_params)
 
-    render json: @properties
+    if @properties.empty?
+      raise ActiveRecord::RecordNotFound.new("No Data")
+    else
+      render json: @properties
+    end
   end
 
   private
@@ -15,5 +21,22 @@ class PropertiesController < ApplicationController
     # other property attributes is optional
     def property_filter_params
       params.slice(:property_type, :marketing_type, :offer_type)
+    end
+
+    # This function to validate search params
+    # location params as mandatory params
+    # type params as optional params
+    def check_search_params
+      filters = [Validate::FilterLocation.new(params), Validate::FilterType.new(params)]
+      errors = {}
+      filters.each do |filter|
+        unless filter.valid?
+          errors[filter.class] = filter.errors
+        end
+      end
+
+      unless errors.blank?
+        render json: { errors: errors }, status: :unprocessable_entity
+      end
     end
 end
